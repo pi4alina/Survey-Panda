4d69b55855769d16d2a4a179b8ab9edd0c74203c
AlinaJahnes & anna karingal <pair+AlinaJahnes.annakaringal@devbootcamp.com>
2015-07-17 18:03:29 -0400

    add routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  resources :users, only: [:create, :show]
+  resources :surveys do
+    resources :questions, except: [:index, :show]
+  end
+  resources :responses, only: [:new, :create]
+  get '/signup' => 'users#new'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
