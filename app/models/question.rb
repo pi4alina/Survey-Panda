9192852f9f2ec136a771255d7990e190756bada1
anna karingal <hello@annakaringal.com>
2015-07-17 19:11:05 -0400

    Add helper methods for possible_values

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,12 @@ class Question < ActiveRecord::Base
   belongs_to :survey
   has_many :responses
   validates :name, presence: true
+
+  def possible_values_to_string(value_array)
+    value_array.join(",")
+  end
+
+  def possible_values_to_array(value_string)
+    value_string.split(",").map(&:strip)
+  end
 end
