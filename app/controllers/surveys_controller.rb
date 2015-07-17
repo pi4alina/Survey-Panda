e2f3206bd43906fa974118133e189f058f81863e
AlinaJahnes & anna karingal <pair+AlinaJahnes.annakaringal@devbootcamp.com>
2015-07-17 19:10:40 -0400

    add crud methods for survey

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,2 +1,29 @@
 class SurveysController < ApplicationController
+  def index
+    @surveys = Survey.all
+  end
+
+  def new
+    @survey = Survey.new
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
 end
