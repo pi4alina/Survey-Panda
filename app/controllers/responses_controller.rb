5e6bdf45439ee50eb113f2adb28b86386e6a6c26
Sihong31 <sihongspring2011@gmail.com>
2015-07-18 15:41:33 -0400

    working radio button form

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,2 +1,23 @@
 class ResponsesController < ApplicationController
+
+  def new
+    @response = Response.new
+    @survey = Survey.find_by(id: params[:id])
+    @question_choices = ["cat","dog","panda","elephant","pineapple"] #hardcoded example
+    # @questions_choices = @survey.questions.possible_values.split
+  end
+
+  def create
+    response = Response.new(response_params)
+    response.question_id = params[:response][:question_id]
+    response.save!
+    render nothing: true
+  end
+
+  private
+
+  def response_params
+    params.require(:response).permit(:content)
+  end
+
 end
