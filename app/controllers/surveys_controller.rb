f7af64501c2e44830271786f8b794fd04d69d49e
AlinaJahnes <alina.jahnes@gamil.com>
2015-07-18 17:10:53 -0400

    create new branch for survey

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,2 +1,28 @@
 class SurveysController < ApplicationController
+  def index
+    @surveys = Survey.all
+  end
+  
+  def new
+    @survey = Survey.new
+  end
+  
+  def create
+    @survey = Survey.new(survey_params)
+    if @survey.save
+      redirect_to new_survey_question_path
+    else
+      flash.now[:error] = @survey.errors.full_messages
+    end
+  end 
+  
+  def show
+    @survey = Survey.find_by(id: params[:id])
+  end
+  
+  private
+  
+  def survey_params
+    params.require(:survey).permit(:title, :description).merge(author_id: session[:user_id])
+  end
 end
